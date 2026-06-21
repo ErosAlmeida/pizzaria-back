a6cd708cf5c0b4d8bc9c919faa56eb292d324a62
ErosAlmeida <eros.almeida.vilela@gmail.com>
2026-06-21 01:28:52 -0300

    user id

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,15 @@ import {CreateUserController} from "./controllers/user/CreateUserController"
 import { validateShema } from "./middlewares/validateSchema";
 import { authUserSchema, createUserSchema } from "./schemas/userSchema";
 import {AuthUserController} from './controllers/user/AuthUserController'
+import { DetailUserController } from "./controllers/user/DetailUserController";
 
 const router = Router()
 
 router.post("/users", validateShema(createUserSchema), new CreateUserController().handle)
     
 router.post("/session",validateShema(authUserSchema), new AuthUserController().handle)
+
+router.get("/me", new DetailUserController().handle)
 
 export {router}
 
