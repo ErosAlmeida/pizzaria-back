0516d443dbac432d54e718c10b42ee36f73d5f85
ErosAlmeida <eros.almeida.vilela@gmail.com>
2026-07-07 19:09:39 -0300

    update

--- src/controllers/order/AddItemController.ts
+++ src/controllers/order/AddItemController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import { AddItemOrderService } from "../../services/order/addItemOrderService";
+import { AddItemOrderService } from "../../services/order/AddItemOrderService";
+
 
 
 class AddItemController{
